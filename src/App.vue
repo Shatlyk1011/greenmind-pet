77e29d9b9b3c3982f71cb5665f3123c4f04d9084
Shatlyk Abdullayev <gj_wp@mail.ru>
2025-07-21 11:14:19 +0500

    rm cl

--- src/App.vue
+++ src/App.vue
@@ -15,21 +15,12 @@
 import { VueLenis, useLenis } from 'lenis/vue' // Also available as global imports, no need to import them manually
 import { watch } from 'vue'
 
-const lenisOptions = {
-  // lenis options (optional)
-}
-
 const lenis = useLenis((lenis) => {
   // called every scroll
-  console.log(lenis)
 })
 
 watch(
   lenis,
-  (lenis) => {
-    // lenis instance
-    console.log(lenis)
-  },
   { immediate: true }
 )
 
